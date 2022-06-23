e903d156ffb48ed81bad2133d9f175f2162f3ad1
Manikanta H <manikantahkadbur733@gmail.com>
2022-06-23 17:49:40 +0530

    uploaded

---
+++ amc_secure_blockchain/emailsetting.py
@@ -0,0 +1,7 @@
+SET_EMAIL_USE_TLS = True
+SET_EMAIL_HOST='smtp.gmail.com'
+SET_EMAIL_HOST_USER='amc9118@gmail.com'
+SET_EMAIL_HOST_PASSWORD='rimjtajydstruehv'
+SET_EMAIL_PORT=587
+SET_EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+SET_DEFAULT_FROM_EMAIL = 'amc9118@gmail.com'
